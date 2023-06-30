11f67bb1128a9f97a9c0532b0f8c4f1497f386e4
Jona Tinoco <tinocojona@outlook.com>
2023-06-30 12:39:30 -0600

    app de tipos de usuarios, modelo UserBaseAccount definido

---
+++ tipos_usuarios/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# Create your models here.
+
+
+class UserBaseAccount(AbstractBaseUser):
+
+    class Categories(models.TextChoices):
+        ADMINISTRADOR = "ADMINISTRADOR", "administrador"
+        USUARIO = "USUARIO", "usuario"
+        CLIENTE = "CLIENTE", "cliente"
+
+    full_name = models.CharField(max_length=127, blank=False, null=False)
+    email = models.EmailField(
+        max_length=64, unique=True, blank=False, null=False)
+    address = models.TextField(max_length=255, blank=False, null=False)
+    company = models.CharField(max_length=64, blank=True, null=True)
+    category = models.CharField(
+        max_length=13, choices=Categories.choices)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    date_birth = models.DateField(blank=False, null=False)
+
+    is_administrador = models.BooleanField(default=False)
+    is_usuario = models.BooleanField(default=False)
+    is_cliente = models.BooleanField(default=False)
+
+    USERNAME_FIELD = ["email"]
+
+    def __str__(self):
+        return str(self.email)
+
