2e973de1ad0522bd7cd2f787826438c6969db39e
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-07-06 20:43:25 -0600

    Clases css añadidas al formulario desde forms.py, template editada para iterar sobre los campos del formulario

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,5 +3,7 @@ from django import forms
 
 class LoginForm(forms.Form):
     email = forms.EmailField(label="Email", max_length=64, required=True)
-    password = forms.CharField(widget=forms.PasswordInput(
-    ), label="Contraseña", min_length=8, required=True)
+    password = forms.CharField(widget=forms.PasswordInput(), label="Contraseña", min_length=8, required=True)
+
+    email.widget.attrs.update({"class": "form-control input-color", "placeholder": "Escribe tu email"})
+    password.widget.attrs.update({"class": "form-control input-color", "placeholder": "Escribe tu contraseña"})
