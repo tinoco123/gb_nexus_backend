46e5a82efc72ee9bb1344a70ffdc8eccab6ad1e0
Jona Tinoco <tinocojona@outlook.com>
2023-07-11 13:57:15 -0600

    formulario en python creado para crear usuario, importado en la vista y renderizado manualmente en el template

---
+++ usuarios_app/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from tipos_usuarios.models import Usuario
+from django.utils.translation import gettext_lazy as _
+
+class CreateUserForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ["email", "password", "first_name", "last_name",
+                  "address", "company", "date_birth"]
+
+        widgets = {
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "password": forms.PasswordInput(attrs={"class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "address": forms.Textarea(attrs={"class": "form-control", "rows": 4}),
+            "company": forms.TextInput(attrs={"class": "form-control"}),
+            "date_birth": forms.DateInput(attrs={"class": "form-control", "type": "date"})
+        }
+        labels = {
+            "first_name": _("Nombre(s)"),
+            "last_name": _("Apellidos"),
+            "address": _("Dirección"),
+            "company": _("Compañia"),
+            "date_birth": _("Fecha de nacimiento"),
+        }
--- usuarios_app/views.py
+++ usuarios_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
+from .forms import CreateUserForm
 from tipos_usuarios.models import Usuario
 from datetime import date, datetime
 import json
@@ -12,10 +13,12 @@ def users(request):
         return HttpResponseForbidden()
     else:
         if request.method == "GET":
+            create_user_form = CreateUserForm()
             users = list(Usuario.objects.values(
                 "id", "first_name", "last_name", "company", "date_joined"))
             usuarios_json = json.dumps(users, default=date_serializer)
-            return render(request, 'usuarios.html', {"users": usuarios_json})
+            return render(request, 'usuarios.html', {"users": usuarios_json,
+                                                     "create_user_form": create_user_form})
 
 
 def date_serializer(obj):
