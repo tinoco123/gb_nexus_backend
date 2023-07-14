5592736fc125ab86b59ce0ec1487f68c112df226
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-07-14 06:46:42 -0600

    Peticion al servidor para crear un usuario y validar si hay errores y ponerlos en el formulario

---
+++ static/usuarios_app/createUser.js
@@ -0,0 +1,42 @@
+function setErrorsInForm(formErrors) {
+  if (formErrors.success === false) {
+    json_errors = JSON.parse(formErrors.errors)
+    for (var error in json_errors) {
+      if (json_errors.hasOwnProperty(error)) {
+        errorName = error
+        var listaErrores = json_errors[error];
+        listaErrores.forEach(function (error) {
+          id = "error_" + errorName
+          errorSpan = document.getElementById(id)
+          errorSpan.innerHTML = error.message
+        });
+      }
+    }
+  }
+}
+
+
+document.getElementById('agregarUsuarioForm').addEventListener('submit', function (event) {
+
+  event.preventDefault();
+  var formData = new FormData(event.target);
+
+  fetch('/create_user/', {
+    method: 'POST',
+    body: formData
+  })
+    .then(function (response) {
+      if (!response.ok) {
+        response.json()
+          .then(function (formErrors) {
+            setErrorsInForm(formErrors)
+          });
+      } else {
+        window.location.href = "/usuarios/"
+      }
+    })
+    .catch(function (error) {
+      console.error('Error:', error);
+    });
+});
+
