42df4d155b7005e0111f8adc6741c3287cd3ca13
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-07-26 16:56:23 -0600

    editar usuario en el backend

--- usuarios_app/urls.py
+++ usuarios_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.users, name='users'),
     path("create/", views.create_user, name='create_user'),
-    path("get/<int:user_id>", views.get_user, name='get_user')
+    path("get/<int:user_id>", views.get_user, name='get_user'),
+    path("edit/<int:user_id>", views.edit_user, name="edit_user")
 ]
--- usuarios_app/views.py
+++ usuarios_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden, JsonResponse, HttpResponseNotAllowed
-from .forms import UserForm
+from .forms import UserForm, EditUserForm
 from tipos_usuarios.models import Usuario
 from datetime import date, datetime
 import json
@@ -39,13 +39,42 @@ def create_user(request):
                 company = user_form.cleaned_data["company"]
                 date_birth = user_form.cleaned_data["date_birth"]
                 Usuario.objects.create(email=email, password=password, first_name=first_name,
-                                            last_name=last_name, address=address, company=company, date_birth=date_birth)
+                                       last_name=last_name, address=address, company=company, date_birth=date_birth)
                 return redirect("users")
             else:
                 errors = user_form.errors.as_json(escape_html=True)
                 return JsonResponse({'success': False, 'errors': errors}, status=400)
         else:
             return HttpResponseNotAllowed(permitted_methods=("POST"))
+
+
+@login_required
+def edit_user(request, user_id):
+    if not request.user.user_type == "ADMINISTRADOR":
+        return HttpResponseForbidden()
+
+    if request.method != "POST":
+        return HttpResponseNotAllowed(permitted_methods=("POST"))
+    else:
+        user = get_object_or_404(Usuario, id=user_id)
+
+        user_form = EditUserForm(request.POST, instance=user)
+        if user_form.is_valid():
+            email = user_form.cleaned_data["email"]
+            password = user_form.cleaned_data["password"]
+            first_name = user_form.cleaned_data["first_name"]
+            last_name = user_form.cleaned_data["last_name"]
+            address = user_form.cleaned_data["address"]
+            company = user_form.cleaned_data["company"]
+            date_birth = user_form.cleaned_data["date_birth"]
+            Usuario.objects.edit(id=user_id, email=email, password=password, first_name=first_name,
+                                   last_name=last_name, address=address, company=company, date_birth=date_birth)
+            return redirect("users")
+        else:
+            errors = user_form.errors.as_json(escape_html=True)
+            print(errors)
+            return JsonResponse({'success': False, 'errors': errors}, status=400)
+
 
 @login_required
 def get_user(request, user_id):
