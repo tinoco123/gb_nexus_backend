f881e9c8775959c05dfad25c45a7586e9570b043
Jona Tinoco <tinocojona@outlook.com>
2023-07-31 17:17:06 -0600

    funcion backend para retornar un result por id

--- resultados_busqueda_app/views.py
+++ resultados_busqueda_app/views.py
@@ -5,7 +5,10 @@ from django.contrib.auth.decorators import login_required
 from mongo_connection.paginator import Pagination
 from dotenv import load_dotenv
 from pymongo.errors import OperationFailure, ServerSelectionTimeoutError, ConnectionFailure
+from bson.errors import InvalidId
 from .utils import MongoJSONEncoder
+from mongo_connection.connection import MongoConnection
+from mongo_connection.search_result_repository import SearchResultRepository
 
 load_dotenv()
 
@@ -32,7 +35,7 @@ def get_page_of_search_results(request):
             page_size = int(request.GET.get("size"))
         except ValueError:
             return HttpResponseBadRequest()
- 
+
         if page_number >= 1 and page_size <= 50:
             try:
                 paginator = Pagination(page_size, str(os.getenv("MONGODB_DATABASE")), str(
@@ -53,3 +56,29 @@ def get_page_of_search_results(request):
                 return HttpResponseServerError()
         else:
             return HttpResponseBadRequest()
+
+
+@login_required
+def get_search_result_by_id(request, id):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden()
+    if request.method != "GET":
+        return HttpResponseNotAllowed(permitted_methods=("GET"))
+    else:
+        try:
+            mongo_client = MongoConnection(str(os.getenv("MONGODB_DATABASE")), str(
+                os.getenv("MONGODB_COLLECTION")))
+            search_result_repo = SearchResultRepository(mongo_client)
+            search_result = search_result_repo.get_by_id(id)
+            if search_result:
+                return JsonResponse(search_result, encoder=MongoJSONEncoder)
+            else:
+                return HttpResponseBadRequest("No se encontró el elemento solicitado")
+        except InvalidId:
+            return HttpResponseBadRequest("El id que solicitaste tiene un formato erroneo")
+        except ServerSelectionTimeoutError:
+            return HttpResponseServerError("El servidor tardo en retornar una respuesta")
+        except ConnectionFailure:
+            return HttpResponseServerError("Error en la conexión a la base de datos")
+        except OperationFailure:
+            return HttpResponseServerError("El servidor fallo en la ejecución de la operación")
