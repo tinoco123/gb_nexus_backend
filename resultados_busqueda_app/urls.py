0e8116c7cfe9c2b0bd9b0d114d8222344803ae74
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-07-31 03:10:32 -0600

    backend for get a page of search results

--- resultados_busqueda_app/urls.py
+++ resultados_busqueda_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.search_results, name='search_results')
+    path("", views.search_results, name='search_results'),
+    path("data/", views.get_page_of_search_results, name='get_page_search_results')
 ]
--- resultados_busqueda_app/views.py
+++ resultados_busqueda_app/views.py
@@ -1,7 +1,49 @@
+import os
+from django.http import HttpResponseForbidden, HttpResponseNotAllowed, JsonResponse, HttpResponseBadRequest, HttpResponseServerError
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from mongo_connection.paginator import Pagination
+from dotenv import load_dotenv
+from pymongo.errors import OperationFailure, ServerSelectionTimeoutError, ConnectionFailure
+load_dotenv()
+
 
 @login_required
 def search_results(request):
-    return render(request, "search_results.html")
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden()
+    if request.method != "GET":
+        return HttpResponseNotAllowed(permitted_methods=("GET"))
+    else:
+        return render(request, "search_results.html")
+
+
+@login_required
+def get_page_of_search_results(request):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden()
+    if request.method != "GET":
+        return HttpResponseNotAllowed(permitted_methods=("GET"))
+    else:
+        page_number = int(request.GET.get("page"))
+        page_size = int(request.GET.get("size"))
+        if page_number >= 0 and page_size <= 50:
+            try:
+                paginator = Pagination(page_size, str(os.getenv("MONGODB_DATABASE")), str(
+                    os.getenv("MONGODB_COLLECTION")))
+                last_page = paginator.last_page
+                documents = paginator.get_page(page_number)
+                search_results = [doc for doc in documents]
+                data_dict = {
+                    "last_page": last_page,
+                    "data": search_results
+                }
+                return JsonResponse(data_dict)
+            except ServerSelectionTimeoutError as e:
+                return HttpResponseServerError()
+            except ConnectionFailure as e:
+                return HttpResponseServerError()
+            except OperationFailure as e:
+                return HttpResponseServerError()
+        else:
+            return HttpResponseBadRequest()
