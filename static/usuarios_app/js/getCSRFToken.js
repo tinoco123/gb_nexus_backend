69f7a7819b3be8cf5ae32f837f753bf53c7ad614
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-11 01:30:32 -0600

    function para obtener csrftoken de las cookies

---
+++ static/usuarios_app/js/getCSRFToken.js
@@ -0,0 +1,7 @@
+function getCSRFToken() {
+    const cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('csrftoken='))
+        .split('=')[1];
+    return cookieValue;
+}
