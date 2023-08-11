59554b84414659d5f4765a66d083f18c9b502676
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-11 01:31:54 -0600

    peticion desde el cliente para eliminar un usuario

--- static/usuarios_app/js/deleteUser.js
+++ static/usuarios_app/js/deleteUser.js
@@ -1,11 +1,30 @@
 // Popular modal eliminar usuario con información del usuario al abrir el modal
 const spanNombreUsuario = document.getElementById("nombre-eliminar")
 const spanMailUsuario = document.getElementById("mail-eliminar")
-const eliminarUsuario = document.getElementById('eliminarUsuarioModal')
-eliminarUsuario.addEventListener('shown.bs.modal', event => {
+const modalEliminarUsuario = document.getElementById("eliminarUsuarioModal")
+const btnEliminarUsuario = document.getElementById("eliminar-usuario-btn")
+
+modalEliminarUsuario.addEventListener('shown.bs.modal', event => {
     var row = table.getRow(rowIndex)
     var nombreUsuario = row.getData().first_name
     var mailUsuario = row.getData().email
     spanNombreUsuario.innerHTML = nombreUsuario
     spanMailUsuario.innerHTML = mailUsuario
+})
+
+btnEliminarUsuario.addEventListener("click", function () {
+    fetch("/users/delete/" + idActualizado, {
+        method: 'DELETE',
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    })
+        .then(function (response) {
+            if (response.ok) {
+                window.location.href = urlUsers;
+            }
+        })
+        .catch(function (error) {
+            console.error('Error:', error);
+        });
 })
