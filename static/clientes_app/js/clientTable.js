39f346aa0f04c31f15777fa8238375b1b75c81f1
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-14 23:49:35 -0600

    mostrar tabla de clientes

---
+++ static/clientes_app/js/clientTable.js
@@ -0,0 +1,24 @@
+var actionIcons = function (cell, formatterParams) {
+    var actionHTML = "<button class='btn px-1 py-0' data-bs-toggle='modal' data-bs-target='#clientModal'><i class='bi bi-pen fs-5'></i></button> <button class='btn px-1 py-0' data-bs-toggle='modal' data-bs-target='#eliminarClienteModal'><i class='bi bi-trash fs-5'></i></button>"
+    return actionHTML;
+};
+
+var table = new Tabulator("#clientTable", {
+    layout: "fitDataFill",
+    data: [],
+    placeholder:"Sin datos que mostrar",
+    columns: [
+        {
+            formatter: "rowSelection", titleFormatter: "rowSelection", hozAlign: "center", headerHozAlign: "center", resizable: false, headerSort: false, cellClick: function (e, cell) {
+                cell.getRow().toggleSelect();
+            }
+        },
+        { title: "ID", field: "id", sorter: "number" },
+        { title: "Nombre", field: "first_name" },
+        { title: "Apellidos", field: "last_name" },
+        { title: "Mail", field: "email"},
+        { title: "Empresa", field: "company" },
+        { title: "Fecha de registro", field: "date_joined"},
+        { title: "Acciones", formatter: actionIcons, hozAlign: "center", headerHozAlign: "center", headerSort: false, width:102 },
+    ],
+});
