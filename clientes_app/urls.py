213c433654806df546b6f5a1606b5ddf8b25c6ff
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-17 22:03:00 -0600

    method create in backend for creating a client

--- clientes_app/urls.py
+++ clientes_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.clients, name="clients"),
+    path("create/", views.create_client, name="create_client"),
     path("data/", views.paginate_clients, name="paginate_clients"),
     path("data/all-clients", views.all_clients, name="all_clients")
 ]
--- clientes_app/views.py
+++ clientes_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from tipos_usuarios.models import Cliente
 from .forms import ClientForm, EditClientForm
 
+
 @login_required
 def clients(request):
     if request.user.user_type == "CLIENTE":
@@ -67,3 +68,27 @@ def paginate_clients(request):
                 return HttpResponseBadRequest("La página solicitada no contiene resultados")
         except TypeError:
             return HttpResponseBadRequest("Los parámetros page y size no deben ser omitidos")
+
+
+@login_required
+def create_client(request):
+    if request.user.user_type != "ADMINISTRADOR":
+        return HttpResponseForbidden("No tienes autorización para acceder a este recurso")
+    if request.method != "POST":
+        return HttpResponseNotAllowed(permitted_methods=("POST"))
+    else:
+        client_form = ClientForm(request.POST)
+        if client_form.is_valid():
+            email = client_form.cleaned_data["email"]
+            password = client_form.cleaned_data["password"]
+            first_name = client_form.cleaned_data["first_name"]
+            last_name = client_form.cleaned_data["last_name"]
+            address = client_form.cleaned_data["address"]
+            company = client_form.cleaned_data["company"]
+            date_birth = client_form.cleaned_data["date_birth"]
+            Cliente.objects.create(email=email, password=password, first_name=first_name,
+                                   last_name=last_name, address=address, company=company, date_birth=date_birth)
+            return JsonResponse({"success": True, "status_text": "Cliente añadido correctamente"}, status=200)
+        else:
+            errors = client_form.errors.as_json(escape_html=True)
+            return JsonResponse({'success': False, 'errors': errors}, status=400)
