fa2058b9a4ff2fcdc8b0d0310889453fe4c8a7c4
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-18 01:49:49 -0600

    evitar que el campo de contraseña de un formulario sea modificado

--- static/base/js/fillForm.js
+++ static/base/js/fillForm.js
@@ -1,6 +1,9 @@
 function fillFormWithData(data, form) {
     for (let i = 0; i < form.elements.length; i++) {
         const field = form[i];
+        if (field.getAttribute("name") === "password"){
+            continue
+        }
         if (field.tagName === 'INPUT' || field.tagName === "TEXTAREA" || field.tagName === "date") {
             if (field.id) {
                 fieldName = field.getAttribute("name")
