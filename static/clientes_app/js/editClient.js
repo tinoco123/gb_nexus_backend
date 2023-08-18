ac8ea831a641c364dc64d792c0a46deb12f645c3
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-18 02:21:48 -0600

    Peticion POST para editar un usuario desde el frontend

--- static/clientes_app/js/editClient.js
+++ static/clientes_app/js/editClient.js
@@ -1,3 +1,4 @@
+// Al abrir el modal prellenar el formulario con los datos del usuario seleccionado
 const editClientModal = document.getElementById('edit-client-modal')
 editClientModal.addEventListener('show.bs.modal', event => {
     setTimeout(() => getClientById(idActualizado), 1)
@@ -22,6 +23,37 @@ function getClientById(user_id) {
                     showNotifications(500, "Error del servidor: El servidor falló al procesar tu solicitud")
                     console.error(error)
                 })
+        })
+        .catch(error => {
+            showNotifications(500, "Error del servidor: El servidor falló al procesar tu solicitud")
+            console.error(error)
+        })
+}
+
+const editClientForm = document.getElementById("edit-client-form").addEventListener("submit", event => {
+    event.preventDefault();
+    var formData = new FormData(event.target);
+    editClient(formData, idActualizado)
+})
+
+function editClient(formData, user_id) {
+    fetch("/clients/edit/" + user_id, {
+        method: "POST",
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = "/clients"
+            } else if (response.status >= 400 || response.status < 500) {
+                response.json()
+                    .then(form_errors => {
+                        setErrorsInForm(form_errors)
+                        showNotifications(response.status, "Error de usuario: Existen errores en tu formulario")
+                    })
+                    .catch(() => {
+                        showNotifications(500, "Error del servidor: El servidor falló al procesar tu solicitud")
+                    })
+            }
         })
         .catch(error => {
             showNotifications(500, "Error del servidor: El servidor falló al procesar tu solicitud")
