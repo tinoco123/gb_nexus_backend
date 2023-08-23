a50c515f5424fdd050ce3ab1898e49a7a692f37b
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-23 08:01:27 -0600

    formulario para una keyword

---
+++ keywords_app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Keyword
+from django.utils.translation import gettext_lazy as _
+
+
+class KeywordForm(forms.ModelForm):
+    class Meta:
+        model = Keyword
+        exclude = ['date_created', 'user']
+
+    widgets = {
+        "first_keyword": forms.TextInput(attrs={"class": "form-control"}),
+        "second_keyword": forms.TextInput(attrs={"class": "form-control"})
+    }
+
+    labels = {
+        "first_keyword": _("Keyword 1"),
+        "second_keyword": _("Keyword 2"),
+    }
