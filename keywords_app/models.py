970737fc94b3a3f193a3236457961d6c99f5af7d
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-23 07:44:42 -0600

    modificacion del modelo keywords y states

--- keywords_app/models.py
+++ keywords_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from tipos_usuarios.models import UserBaseAccount
 
-# Create your models here.
+
+from django.db import models
+
+
+class States(models.Model):
+    state = models.CharField(max_length=30 ,unique=True)
+
+
+class Keyword(models.Model):
+    first_keyword = models.CharField(max_length=50, null=False, blank=False)
+    second_keyword = models.CharField(max_length=50, null=True, blank=True)
+    states_to_search = models.ManyToManyField(States)
+    date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(UserBaseAccount, on_delete=models.CASCADE, null=False)
