4a56c1b389e5815e52fb3f416ce3cdf35bb68068
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-25 16:16:47 -0600

    Mostrar datos del resultado de busqueda en modal ver mas

--- static/resultados_busqueda_app/js/getResult.js
+++ static/resultados_busqueda_app/js/getResult.js
@@ -1,10 +1,19 @@
-var idActualizado
+const pageSpan = document.getElementById("id-page")
+const urlSpan = document.getElementById("id-url")
+const stateSpan = document.getElementById("id-state")
+const sinopsysSpan = document.getElementById("id-sinopsys")
+const dateSpan = document.getElementById("id-date")
+const zoneSpan = document.getElementById("id-zone")
 
-table.on("rowClick", function (e, row) {
-    idActualizado = row.getData()._id
-})
+const modalVerResultadosBusqueda = document.getElementById("modalVerResultadosBusqueda")
 
-modalVerResultadosBusqueda = document.getElementById("modalVerResultadosBusqueda")
-modalVerResultadosBusqueda.addEventListener('shown.bs.modal', event => {
-
+modalVerResultadosBusqueda.addEventListener('show.bs.modal', event => {
+    setTimeout(()=> {
+        pageSpan.innerHTML = rowSelected.getData().title
+        urlSpan.innerHTML = rowSelected.getData().urlPage
+        urlSpan.setAttribute("href", rowSelected.getData().urlPage)
+        stateSpan.innerHTML = rowSelected.getData().state
+        dateSpan.innerHTML = rowSelected.getData().date
+        zoneSpan.innerHTML = rowSelected.getData().federalEstatal
+    }, 1)
 })
