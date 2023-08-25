965a337a452251b3e69ac2cd52f8706a9063b011
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-25 17:24:32 -0600

    Mostrar sinopsys en modal ver mas del modulo de resultados de busqueda

--- static/resultados_busqueda_app/js/getResult.js
+++ static/resultados_busqueda_app/js/getResult.js
@@ -8,12 +8,34 @@ const zoneSpan = document.getElementById("id-zone")
 const modalVerResultadosBusqueda = document.getElementById("modalVerResultadosBusqueda")
 
 modalVerResultadosBusqueda.addEventListener('show.bs.modal', event => {
-    setTimeout(()=> {
+    setTimeout(() => {
         pageSpan.innerHTML = rowSelected.getData().title
         urlSpan.innerHTML = rowSelected.getData().urlPage
         urlSpan.setAttribute("href", rowSelected.getData().urlPage)
         stateSpan.innerHTML = rowSelected.getData().state
         dateSpan.innerHTML = rowSelected.getData().date
         zoneSpan.innerHTML = rowSelected.getData().federalEstatal
+        getSinopsys()
+            .then(data => {
+                if (data) {
+                    sinopsysSpan.innerHTML = data.sinopsys
+                }
+            });
     }, 1)
 })
+
+async function getSinopsys() {
+    try {
+        const searchResultId = rowSelected.getData()._id;
+        const response = await fetch("/search-results/data/get/" + searchResultId, {
+            method: "GET"
+        });
+        if (!response.ok) {
+            throw new Error('Error en la petición al servidor');
+        }
+        const jsonData = await response.json();
+        return jsonData
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
