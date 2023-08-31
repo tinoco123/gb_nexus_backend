ba53f8699ee2959b34ee3237799e2e749daa2d04
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-08-31 11:41:17 -0600

    Modificacion al modelo keyword: Dependencias federalesagregadas

--- keywords_app/models.py
+++ keywords_app/models.py
@@ -12,11 +12,20 @@ class States(models.Model):
         return self.state
 
 
+class Federal(models.Model):
+    federal = models.CharField(max_length=31, unique=True)
+
+    def __str__(self):
+        return self.federal
+
+
 class Keyword(models.Model):
     first_keyword = models.CharField(max_length=50, null=False, blank=False)
     second_keyword = models.CharField(max_length=50, null=True, blank=True)
-    congreso_search  = models.ManyToManyField(States)
-    estatal_search  = models.ManyToManyField(States, related_name="estatal_search")
+    congreso_search = models.ManyToManyField(States)
+    estatal_search = models.ManyToManyField(
+        States, related_name="estatal_search")
+    federal_search = models.ManyToManyField(Federal)
     date_created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         UserBaseAccount, on_delete=models.CASCADE, null=False)
@@ -29,7 +38,8 @@ class Keyword(models.Model):
             "first_keyword": self.first_keyword,
             "second_keyword": self.second_keyword,
             "congreso_search": list(self.congreso_search.all().values_list("id", flat=True)),
-            "estatal_search": list(self.estatal_search.all().values_list("id", flat=True))
+            "estatal_search": list(self.estatal_search.all().values_list("id", flat=True)),
+            "federal_search": list(self.federal_search.all().values_list("id", flat=True))
         }
         return keyword_json
 
@@ -41,12 +51,15 @@ class Keyword(models.Model):
         else:
             query_keywords = {"$regex": self.first_keyword, "$options": "i"}
 
-        congreso_states = tuple(self.congreso_search.all().values_list("state", flat=True))
+        congreso_states = tuple(
+            self.congreso_search.all().values_list("state", flat=True))
         query_states = {}
         if len(congreso_states) == 1:
-            query_states = {"$regex": "".join(congreso_states), "$options": "i"}
+            query_states = {"$regex": "".join(
+                congreso_states), "$options": "i"}
         else:
-            query_states = {"$regex": "|".join(congreso_states), "$options": "i"}
+            query_states = {"$regex": "|".join(
+                congreso_states), "$options": "i"}
 
         query = {
             "$and": [
