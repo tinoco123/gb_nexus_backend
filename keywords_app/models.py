700afd63edd2bee4b3d7e07b2ce48d70508822d9
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-09-04 10:52:43 -0600

    Refactor modelo keywords: Terminos de busqueda

--- keywords_app/models.py
+++ keywords_app/models.py
@@ -20,8 +20,6 @@ class Federal(models.Model):
 
 
 class Keyword(models.Model):
-    first_keyword = models.CharField(max_length=50, null=False, blank=False)
-    second_keyword = models.CharField(max_length=50, null=True, blank=True)
     congreso_search = models.ManyToManyField(States)
     estatal_search = models.ManyToManyField(
         States, related_name="estatal_search")
@@ -30,13 +28,9 @@ class Keyword(models.Model):
     user = models.ForeignKey(
         UserBaseAccount, on_delete=models.CASCADE, null=False)
 
-    def __str__(self):
-        return self.first_keyword
 
     def to_json(self):
         keyword_json = {
-            "first_keyword": self.first_keyword,
-            "second_keyword": self.second_keyword,
             "congreso_search": list(self.congreso_search.all().values_list("id", flat=True)),
             "estatal_search": list(self.estatal_search.all().values_list("id", flat=True)),
             "federal_search": list(self.federal_search.all().values_list("id", flat=True))
@@ -44,32 +38,10 @@ class Keyword(models.Model):
         return keyword_json
 
     def query(self):
-        query_keywords = {}
-        if self.second_keyword:
-            query_keywords = {"$regex": "|".join(
-                (self.first_keyword, self.second_keyword)), "$options": "i"}
-        else:
-            query_keywords = {"$regex": self.first_keyword, "$options": "i"}
+        return
 
-        congreso_states = tuple(
-            self.congreso_search.all().values_list("state", flat=True))
-        query_states = {}
-        if len(congreso_states) == 1:
-            query_states = {"$regex": "".join(
-                congreso_states), "$options": "i"}
-        else:
-            query_states = {"$regex": "|".join(
-                congreso_states), "$options": "i"}
 
-        query = {
-            "$and": [
-                {
-                    "$or": [
-                        {"sinopsys": query_keywords},
-                        {"urlAttach.sinopsys": query_keywords}
-                    ]
-                },
-                {"state": query_states}
-            ]
-        }
-        return query
+class SearchTerms(models.Model):
+    name = models.CharField(max_length=30)
+    is_required = models.BooleanField()
+    keyword = models.ForeignKey(Keyword, on_delete=models.CASCADE)
