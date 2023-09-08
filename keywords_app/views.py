4b055d7e11c788f68d73235f5113199bcb10ce2f
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-09-08 12:23:08 -0600

    Metodo agregar nueva keyword actualizado con nueva estructura del modelo para una keyword

--- keywords_app/views.py
+++ keywords_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotAllowed, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage
-from keywords_app.models import Keyword
+from keywords_app.models import Keyword, SearchTerms
 from .forms import KeywordForm, EditKeywordForm
 from tipos_usuarios.models import UserBaseAccount
 from django.contrib.auth.decorators import login_required
@@ -34,11 +34,23 @@ def create_keyword(request):
                 keyword_form.cleaned_data["estatal_search"])
             keyword.federal_search.set(
                 keyword_form.cleaned_data["federal_search"])
-            return JsonResponse({"success": True, "status_text": "Keyword añadido correctamente"}, status=200)
+
+            set_search_terms_to_keyword(keyword_form, keyword)
+
+            return JsonResponse({}, status=200)
         else:
             errors = keyword_form.errors.as_json(escape_html=True)
             return JsonResponse({'success': False, 'errors': errors}, status=400)
 
+
+def set_search_terms_to_keyword(keyword_form: KeywordForm, keyword: Keyword):
+    search_terms = []
+    for i in range(1, 6):
+        search_term = keyword_form.cleaned_data[f"search_term_{i}"]
+        filter = keyword_form.cleaned_data[f"filter_{i}"]
+        if search_term and filter:
+            search_terms.append(SearchTerms(name=search_term, is_required=filter, keyword=keyword))
+    SearchTerms.objects.bulk_create(search_terms)
 
 @login_required
 def paginate_keywords(request):
@@ -106,14 +118,17 @@ def edit_keyword(request, keyword_id):
         edit_keyword_form = EditKeywordForm(request.POST, instance=keyword)
         if edit_keyword_form.is_valid():
             keyword = edit_keyword_form.save()
-            keyword.congreso_search.set(edit_keyword_form.cleaned_data["congreso_search"])
-            keyword.estatal_search.set(edit_keyword_form.cleaned_data["estatal_search"])
-            keyword.federal_search.set(edit_keyword_form.cleaned_data["federal_search"])
+            keyword.congreso_search.set(
+                edit_keyword_form.cleaned_data["congreso_search"])
+            keyword.estatal_search.set(
+                edit_keyword_form.cleaned_data["estatal_search"])
+            keyword.federal_search.set(
+                edit_keyword_form.cleaned_data["federal_search"])
             return JsonResponse({"success": True, "status_text": "Keyword editado correctamente"}, status=200)
         else:
             errors = edit_keyword_form.errors.as_json(escape_html=True)
             return JsonResponse({'success': False, 'errors': errors}, status=400)
-        
+
 
 @login_required
 def delete_keyword(request, keyword_id):
