91b0886857c162c11e8fa55f01172484a999bfab
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-09-21 17:44:32 -0600

    consulta para obtener urlAttach de un resultado de busqueda

--- mongo_connection/search_result_repository.py
+++ mongo_connection/search_result_repository.py
@@ -1,5 +1,6 @@
 from bson import ObjectId
 
+
 class SearchResultRepository:
     def __init__(self, mongo_client):
         self.mongo_client = mongo_client
@@ -7,5 +8,11 @@ class SearchResultRepository:
 
     def get_by_id(self, id):
         id = ObjectId(id)
-        search_result = self.collection.find_one({"_id": id}, {"sinopsys": 1, "urlAttach": 1})
+        search_result = self.collection.find_one(
+            {"_id": id}, {"sinopsys": 1, "urlAttach": 1})
         return search_result
+
+    def get_url_attach_by_id(self, id):
+        id = ObjectId(id)
+        urlAttach = self.collection.find_one({"_id": id}, {"urlAttach": 1})
+        return urlAttach
