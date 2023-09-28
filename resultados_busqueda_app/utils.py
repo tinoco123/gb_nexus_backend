581294d8eda5b70a2ff74aa18450d6fc75fe4e30
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-09-28 16:41:29 -0600

    serializer for datetime in searc results

--- resultados_busqueda_app/utils.py
+++ resultados_busqueda_app/utils.py
@@ -1,8 +1,12 @@
 import json
 from bson import ObjectId
+from datetime import datetime, date
+
 
 class MongoJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
+        if isinstance(o, (datetime, date)):
+            return o.isoformat()
         return json.JSONEncoder.default(self, o)
