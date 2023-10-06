20b8f1c0fbb7112b270dfacfd415c767c1486eb2
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-10-06 17:22:32 -0600

    Funcion para asignar links a cada keyword de los resultados de busqueda

--- static/resultados_busqueda_app/js/keywordList.js
+++ static/resultados_busqueda_app/js/keywordList.js
@@ -1,21 +1,25 @@
 var keyword
-var keywordLinks = document.querySelectorAll(".keyword-link")
 
-if (keywordLinks.length > 0) {
-    setKeyword(keywordLinks[0].id)
-    keywordLinks.forEach(function (link) {
-        link.addEventListener("click", function (event) {
-            event.preventDefault()
-            setKeyword(link.getAttribute("id"))
-            table.setData("/search-results/data/", { keyword: getKeyword() })
-                .then(() => {
-                    showNotifications(200, "Resultados de keyword: " + link.textContent)
-                })
-            event.stopPropagation()
+function assignLinksToEachKeyword(params) {
+    var keywordLinks = document.querySelectorAll(".keyword-link")
+
+    if (keywordLinks.length > 0) {
+        setKeyword(keywordLinks[0].id)
+        keywordLinks.forEach(function (link) {
+            link.addEventListener("click", function (event) {
+                event.preventDefault()
+                setKeyword(link.getAttribute("id"))
+                table.setData("/search-results/data/", { keyword: getKeyword() })
+                    .then(() => {
+                        showNotifications(200, "Resultados de keyword: " + link.textContent)
+                    })
+                event.stopPropagation()
+            })
         })
-    })
-} else {
-    table.setData("/search-results/data/", { keyword: 0 })
+    } else {
+        table.setData("/search-results/data/", { keyword: 0 })
+    }
+
 }
 
 
