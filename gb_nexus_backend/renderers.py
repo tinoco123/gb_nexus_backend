af3989e48faba76e000bc48b00440bf361eabf16
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-10-06 12:09:50 -0600

    utf-8, ahora se pueden ver acentos en los pdfs

--- gb_nexus_backend/renderers.py
+++ gb_nexus_backend/renderers.py
@@ -8,7 +8,7 @@ def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html  = template.render(context_dict)
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    pdf = pisa.pisaDocument(BytesIO(html.encode("utf-8")), result)
     if pdf.err:
         return HttpResponse("Invalid PDF", status_code=400, content_type='text/plain')
     return HttpResponse(result.getvalue(), content_type='application/pdf')
