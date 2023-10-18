96a33a61c597f3d74f56b8778bc38a1b86def9a4
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-10-18 15:12:21 -0600

    Bug corregido en modal editar

--- static/keywords_app/js/manipulateKeywordsUIEditModal.js
+++ static/keywords_app/js/manipulateKeywordsUIEditModal.js
@@ -1,9 +1,32 @@
+var boxesWithData = []
+var emptyBoxes = []
+
+const addKeywordModalEdit = document.getElementById("add-keyword-modal-edit")
+const deleteKeywordModalEdit = document.getElementById("delete-keyword-modal-edit")
+
+addKeywordModalEdit.addEventListener("click", () => {
+    if (emptyBoxes.length >= 1) {
+        var boxInputDeleted = emptyBoxes.shift()
+        boxInputDeleted.removeAttribute("hidden")
+        boxesWithData.push(boxInputDeleted)
+    }
+})
+
+deleteKeywordModalEdit.addEventListener("click", () => {
+    if (boxesWithData.length >= 1) {
+        var boxInputDeleted = boxesWithData.pop()
+        boxInputDeleted.setAttribute("hidden", "")
+        emptyBoxes.unshift(boxInputDeleted)
+        var input = boxInputDeleted.querySelector("input")
+        input.value = ""
+    }
+})
+
 function manipulateKeywordsUIEditModal() {
     var editBoxes = document.getElementsByClassName("edit-box")
-
     editBoxes = Array.from(editBoxes)
-    var boxesWithData = []
-    var emptyBoxes = editBoxes.filter(editBox => {
+    boxesWithData = []
+    emptyBoxes = editBoxes.filter(editBox => {
         var editBoxInputValue = editBox.querySelector("input").value
         if (editBoxInputValue.length != 0) {
             boxesWithData.push(editBox)
@@ -11,27 +34,6 @@ function manipulateKeywordsUIEditModal() {
         return editBoxInputValue.length === 0
     })
     showBoxesWithData(boxesWithData)
-
-    const addKeywordModalEdit = document.getElementById("add-keyword-modal-edit")
-    const deleteKeywordModalEdit = document.getElementById("delete-keyword-modal-edit")
-
-    addKeywordModalEdit.addEventListener("click", () => {
-        if (emptyBoxes.length >= 1) {
-            var boxInputDeleted = emptyBoxes.shift()
-            boxInputDeleted.removeAttribute("hidden")
-            boxesWithData.push(boxInputDeleted)
-        }
-    })
-
-    deleteKeywordModalEdit.addEventListener("click", () => {
-        if (boxesWithData.length >= 1) {
-            var boxInputDeleted = boxesWithData.pop()
-            boxInputDeleted.setAttribute("hidden", "")
-            emptyBoxes.unshift(boxInputDeleted)
-            var input = boxInputDeleted.querySelector("input")
-            input.value = ""
-        }
-    })
 }
 
 function showBoxesWithData(boxesWithData) {
