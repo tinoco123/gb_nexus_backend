f8a41e1da4ecc25b1c25987d84964be77f54ec8d
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-11-08 16:08:55 -0600

    Cambio de nombre del software

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.sign_in, name='sign_in'),
     path("login/", views.sign_in, name='sign_in'),
     path('logout/', views.sign_out, name='sign_out')
 ]
